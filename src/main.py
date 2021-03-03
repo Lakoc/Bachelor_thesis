32477d333c812773a054df60705909d17ac9c9ed
lakoc <xpolok03@stud.fit.vutbr.cz>
2021-03-03 16:04:11 +0100

    general clean up

--- src/main.py
+++ src/main.py
@@ -26,30 +26,32 @@ wav_file, sampling_rate = read_wav_file(params.file_path)
 remove_pitches_size = int(params.remove_pitches_size / params.window_size)
 hesitations_max_size_max_size = int(params.hesitations_max_size_max_size / params.window_size)
 
-if params.with_gmm:
-    vad = process_voice_activity_detection_via_gmm(wav_file, remove_pitches_size, win_length=sampling_rate // 40)
-else:
-    # get parameters needed for next processing
-    segmented_tracks = process_hamming(wav_file, params.pre_emphasis_coefficient, sampling_rate, params.window_size,
-                                       params.overlap_size)
-    energy_over_segments = calculate_energy_over_segments(segmented_tracks, params.displayEnergy)
-    zero_crossings = calculate_sign_changes(segmented_tracks)
-    vad = process_voice_activity_detection_via_energy(energy_over_segments, zero_crossings, params.energy_threshold,
-                                                      remove_pitches_size)
-    cross_talks, interruptions = statistics.detect_cross_talks(vad)
-    vad_joined, hesitations = statistics.join_vad_and_count_hesitations(vad, hesitations_max_size_max_size)
+# TODO: VAD based on energy
 
-    # generate statistics
-    mean_energies = statistics.count_mean_energy_when_speaking(energy_over_segments, vad)
-    speech_time = statistics.get_speech_time(vad)
-    responses, sentences_lengths = statistics.generate_response_and_speech_time_statistics(vad)
-    # lpc = statistics.calculate_lpc_over_segments(segmented_tracks, number_of_lpc_coefficients)
-    # # TODO: Test lpc
-    # for i in range(100, 110):
-    #     outputs.plot_lpc_ftt(segmented_tracks[0][i], sampling_rate, lpc[0][i], number_of_lpc_coefficients)
-
-    # # plot outputs
-    outputs.plot_wav_with_detection(sampling_rate, wav_file, vad, vad_joined, cross_talks, params.save_to)
+# if params.with_gmm:
+#     vad = process_voice_activity_detection_via_gmm(wav_file, remove_pitches_size, win_length=sampling_rate // 40)
+# else:
+#     # get parameters needed for next processing
+#     segmented_tracks = process_hamming(wav_file, params.pre_emphasis_coefficient, sampling_rate, params.window_size,
+#                                        params.overlap_size)
+#     energy_over_segments = calculate_energy_over_segments(segmented_tracks, params.displayEnergy)
+#     zero_crossings = calculate_sign_changes(segmented_tracks)
+#     vad = process_voice_activity_detection_via_energy(energy_over_segments, zero_crossings, params.energy_threshold,
+#                                                       remove_pitches_size)
+#     cross_talks, interruptions = statistics.detect_cross_talks(vad)
+#     vad_joined, hesitations = statistics.join_vad_and_count_hesitations(vad, hesitations_max_size_max_size)
+#
+#     # generate statistics
+#     mean_energies = statistics.count_mean_energy_when_speaking(energy_over_segments, vad)
+#     speech_time = statistics.get_speech_time(vad)
+#     responses, sentences_lengths = statistics.generate_response_and_speech_time_statistics(vad)
+#     # lpc = statistics.calculate_lpc_over_segments(segmented_tracks, number_of_lpc_coefficients)
+#     # # Test lpc
+#     # for i in range(100, 110):
+#     #     outputs.plot_lpc_ftt(segmented_tracks[0][i], sampling_rate, lpc[0][i], number_of_lpc_coefficients)
+#
+#     # # plot outputs
+#     outputs.plot_wav_with_detection(sampling_rate, wav_file, vad, vad_joined, cross_talks, params.save_to)
     # outputs.plot_speech_time_comparison(mean_energies, speech_time, save_to)
     # outputs.plot_interruptions(interruptions, save_to)
     # outputs.plot_responses_lengths(responses, sentences_lengths, save_to)
